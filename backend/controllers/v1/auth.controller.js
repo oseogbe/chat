82166bf48dd121208442ad6e4e581e17ee89ca8f
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-01-12 17:07:00 +0100

    add user id to login response

--- backend/controllers/v1/auth.controller.js
+++ backend/controllers/v1/auth.controller.js
@@ -20,7 +20,8 @@ class AuthController {
       }
 
       const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await prisma.user.create({
+
+      await prisma.user.create({
         data: {
           name,
           email,
@@ -29,9 +30,7 @@ class AuthController {
         },
       });
 
-      res
-        .status(201)
-        .json({ message: "User created successfully", userId: user.id });
+      res.status(201).json({ message: "User created successfully" });
     } catch (err) {
       res.status(500).json({ error: "Internal server error" });
     }
@@ -61,7 +60,7 @@ class AuthController {
         expiresIn: "1h",
       });
 
-      res.status(200).json({ token });
+      res.status(200).json({ userId: user.id, token });
     } catch (err) {
       res.status(500).json({ error: "Internal server error" });
     }
