71a165651c42bf0627485f442446ece548bc900e
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-01-13 22:25:30 +0100

    update Home component to handle contact selection

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,15 +2,19 @@
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
+
 import Topbar from '@/components/chat/Topbar';
 import ChatMenu from '@/components/chat/ChatMenu';
 import ContactDetailsMenu from '@/components/chat/ContactDetailsMenu';
 import ChatInput from '@/components/chat/ChatInput';
 import ChatMessages from '@/components/chat/ChatMessages';
 
+import { Contact } from '@/typings';
+
 const Home = () => {
   const { data: session } = useSession();
   const [isContactDetailsMenuOpen, setContactDetailsMenuOpen] = useState(false);
+  const [selectedContact, setSelectedContact] = useState<Contact>();
 
   if (!session) {
     return null;
@@ -24,17 +28,31 @@ const Home = () => {
     setContactDetailsMenuOpen(false);
   };
 
+  const handleSelectContact = (contact: Contact) => {
+    setSelectedContact(contact);
+  };
+
   return (
     <div className='h-screen flex flex-1'>
-      <ChatMenu />
-      <div className='relative flex flex-col flex-1'>
-        <Topbar onClick={openContactDetailsMenu} />
-        <div className='relative bg-[#f5f5f5] flex-1 pb-24 overflow-y-auto scrollbar-hide'>
-          <ChatMessages />
-        </div>
-        <ChatInput />
-      </div>
-      <ContactDetailsMenu isOpen={isContactDetailsMenuOpen} onClose={closeContactDetailsMenu} />
+      <ChatMenu onSelectContact={handleSelectContact} />
+      {
+        !selectedContact ? (
+          <div className='bg-[#f5f5f5] h-screen flex flex-1'></div>
+        ) : (
+          <div className='relative flex flex-col flex-1'>
+            <Topbar onClick={openContactDetailsMenu} contactName={selectedContact?.name} />
+            <div className='relative bg-[#f5f5f5] flex-1 pb-24 overflow-y-auto scrollbar-hide'>
+              <ChatMessages />
+            </div>
+            <ChatInput />
+          </div>
+        )
+      }
+      <ContactDetailsMenu
+        isOpen={isContactDetailsMenuOpen}
+        onClose={closeContactDetailsMenu}
+        contact={selectedContact}
+      />
     </div>
   )
 }
