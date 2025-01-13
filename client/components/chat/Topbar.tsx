0b4ee2e9b4aceda67792c3eaaded92b60beb75d7
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-01-13 10:13:52 +0100

    prevent shrinking

--- client/components/chat/Topbar.tsx
+++ client/components/chat/Topbar.tsx
@@ -6,7 +6,7 @@ interface TopbarProps {
 
 const Topbar: React.FC<TopbarProps> = ({ onClick }) => {
     return (
-        <div className="w-full h-[70px] border-b border-[#D9DCE0] shadow-sm flex items-center px-4">
+        <div className="w-full h-[70px] shrink-0 border-b border-[#D9DCE0] shadow-sm flex items-center px-4">
             <ContactDetails onClick={onClick} />
         </div>
     )
