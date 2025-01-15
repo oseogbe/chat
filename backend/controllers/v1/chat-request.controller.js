9b93773529fac72f780f934ab5bcb1f81250c8e2
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-01-15 09:32:54 +0100

    get chat requests for a user

--- backend/controllers/v1/chat-request.controller.js
+++ backend/controllers/v1/chat-request.controller.js
@@ -1,6 +1,54 @@
 const { prisma } = require("../../lib/prisma");
 
 class ChatRequestController {
+  async getAllChatRequests(req, res) {
+    const { status } = req.query;
+
+    const filter = {
+      OR: [
+        {
+          senderId: req.user.userId,
+        },
+        {
+          receiverId: req.user.userId,
+        },
+      ],
+    };
+
+    if (status) {
+      if (!["PENDING", "ACCEPTED", "REJECTED"].includes(status)) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid status.",
+        });
+      }
+      filter.status = status;
+    }
+
+    try {
+      const chatRequests = await prisma.chatRequest.findMany({
+        where: filter,
+        include: {
+          sender: true,
+          receiver: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      return res.status(200).json({
+        success: true,
+        data: chatRequests,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+
   async sendChatRequest(req, res) {
     const senderId = req.user.userId;
     const { receiverId } = req.body;
--- backend/routes/v1/chat-request.route.js
+++ backend/routes/v1/chat-request.route.js
@@ -6,6 +6,7 @@ const chatRequestController = new ChatRequestController();
 
 const router = express.Router();
 
+router.get("/all", authenticate, chatRequestController.getAllChatRequests);
 router.post("/send", authenticate, chatRequestController.sendChatRequest);
 router.post(
   "/update-status",
